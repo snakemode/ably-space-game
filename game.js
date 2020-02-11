4bea86d76b54fec35dc6e63d9c7f315f850ab00b
Glitch (florentine-crayfish-xqtpugytnl) <none>
2020-02-11 10:20:19 +0000

    💃👀 Checkpoint ./game.js:7701387/2822

--- game.js
+++ game.js
@@ -5,33 +5,43 @@ class Game {
         this.id = this.___uuidv4();
         this.moves = [
             0
-        ]; // Randomly pick a selection of move ids
+        ]; // Randomly pick a selection of move ids at the start of each game
     }
 
     handleMove(state, extraParams) {
-        const nextMoveId = this.moves[this.moves.length-1];
 
-        const matcher = allMoves[nextMoveId].succeedsWhen;
-        const moveResult = matcher(state, extraParams);
-        console.log(moveResult);
-        
-        if (moveResult === true) {
-            this.moves(pop);
+        let activeMoveId = this.activeMoveId();
+        let currentMove = this.getMove(activeMoveId);        
+        const moveResult =  currentMove.succeedsWhen(state, extraParams);
+
+        if (moveResult !== true) {
+            console.log("Move failed.");
+            return { game: "in-progress", movesLeft: this.moves.length, hint: currentMove.hintText }; 
         }
 
-        if (this.moves.length === 0) {
-            return { game: "complete", movesLeft: this.moves.length };
-        }
+        if (this.gameIsFinished()) {
+            console.log("Game completed!");
+            return { game: "complete", movesLeft: this.moves.length, hint: ""  };
+        }        
         
-        const activeMove = this.moves[this.moves.length-1];
-        this.sendNextHint(activeMove.hint);
+        console.log("Move was a success!");
+        this.markCurrentMoveAsCompleted();
 
-        return { game: "in-progress", movesLeft: this.moves.length, hint: activeMove.hintText };        
+        activeMoveId = this.activeMoveId();
+        currentMove = this.getMove(activeMoveId);
+        this.sendNextHint(currentMove.hint);
+
+        return { game: "in-progress", movesLeft: this.moves.length, hint: currentMove.hintText };        
     }
 
     sendNextHint(hintText) {
         // push over channel to send text message
     }
+
+    gameIsFinished() { return this.moves.length === 0; }
+    markCurrentMoveAsCompleted() { this.moves(pop); }
+    activeMoveId() { return this.moves[this.moves.length-1]; }
+    getMove(moveId) { return allMoves[moveId]; }
 
     ___uuidv4() {
         return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
