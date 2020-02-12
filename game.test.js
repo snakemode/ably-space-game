3bf85915ba3989ef81452ce76ca3eab3196be827
Glitch (florentine-crayfish-xqtpugytnl) <none>
2020-02-12 13:52:44 +0000

    😻🌼 Checkpoint ./game.test.js:7701387/2290

--- game.test.js
+++ game.test.js
@@ -11,9 +11,33 @@ describe("Game", () => {
         expect(sut).not.toBeNull();
     });
   
-    it("accepts collection of move IDs in the ctor", () => {
+    it("accepts collection of move IDs in the constructor", () => {
         const sut = new Game("1234", () => { }, [ 0 ]);      
         expect(sut.moves.length).toBe(1);
     });
+  
+    it("executes state-changed callback when the class constructs, starting the game", () => {
+        let called = false;
+      
+        const sut = new Game("1234", () => {
+          called = true;
+        }, [ 0 ]);  
+      
+        expect(called).toBe(true);
+    });
 
+    it("generates ten random moves if none are provided", () => {
+        const sut = new Game("1234", () => { });      
+        expect(sut.moves.length).toBe(10);
+    });
+  
+    it("generates an id on construction", () => {
+        const sut = new Game("1234", () => { });      
+        expect(sut.id).not.toBeNull();
+    });
+  
+    it("generates a game expiry time in the future", () => {
+        const sut = new Game("1234", () => { });      
+        expect(sut.id).not.toBeNull();
+    });
 });
