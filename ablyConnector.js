dda8ff0a29437ba9cba6ef8ff03fd5d043a4e5d8
Glitch (ably-space-game) <none>
2020-02-25 10:16:48 +0000

    💸👸 Checkpoint ./ablyConnector.js:562062/439 ./package.json:562062/48

--- ablyConnector.js
+++ ablyConnector.js
@@ -1,6 +1,4 @@
 const ably = require('ably');
-const fetch = require('node-fetch');
-const apiUrl = "https://rest.ably.io/channels/space-game/messages?key=" + process.env.ABLY_API_KEY;
 const enabled = true;
 
 async function onGameStateChanged(status) {
@@ -13,23 +11,10 @@ async function onGameStateChanged(status) {
 }
 
 async function sendToApi(jsonBody) {
-  let client = new ably.Realtime(process.env.ABLY_API_KEY)
-
+  const client = new ably.Realtime(process.env.ABLY_API_KEY)
   const channel = client.channels.get('space-game');
 
   channel.publish('ably-space-game', jsonBody);  
-  
-//     try {
-//       await fetch(apiUrl, {
-//         method: 'POST',
-//         headers: { 'Content-Type': 'application/json' },
-//         body: JSON.stringify(jsonBody)
-//       });
-
-//       console.log("Sent to Ably");
-//     } catch (error) {
-//       console.log(error);
-//     }
 }
 
 module.exports = onGameStateChanged;
